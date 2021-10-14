e8f46524b5086cfabeee8cd5ef8a7be6562c19f5
farshad <farshadnp6@gmail.com>
2021-10-14 05:47:49 +0330

    Add project files.

---
+++ BookShop/Models/BookShopContext.cs
@@ -0,0 +1,40 @@
+﻿using BookShop.Mapping;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookShop.Models;
+
+namespace BookShop.Models
+{
+    public class BookShopContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(local);Database=BookShopDB;Trusted_Connection=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new Author_BookMap());
+            modelBuilder.ApplyConfiguration(new CustomerMap());
+            modelBuilder.ApplyConfiguration(new Order_BookMap());
+            modelBuilder.ApplyConfiguration(new Book_TranslatorMap());
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Provice> Provices { get; set; }
+        public DbSet<Author_Book> Author_Books { get; set; }
+        public DbSet<Order_Book> Order_Books { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<OrderStatus> OrderStatuses { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<BookShop.Models.Translator> Translator { get; set; }
+    }
+}
